450baa1e6dfe9dbdea370b47965a4dc6f8dbff6f
muneneevans <evans@vorane.com>
2023-09-22 19:54:19 +0300

    ADD draggable card #20 Lesson-20

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 export default function Deck({ data, renderCard }) {
+
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gesture) => {
+            // position.setValue({ x: gesture.dx, y: gesture.dy });
+            // debugger
+            position.setValue({ x: gesture.dx, y: gesture.dy })
+        },
+        onPanResponderRelease: () => { },
+    });
+
     const renderCards = () => {
-        return data.map(item => renderCard(item))
+        return data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View style={position.getLayout()} {...panResponder.panHandlers} key={item.id}>
+                        {renderCard(item)}
+                    </Animated.View>)
+            }
+            else {
+                return renderCard(item)
+            }
+        })
     }
+
     return (
-        <Animated.View >
+        <View >
             {renderCards()}
-        </Animated.View>
+        </View>
     );
 }
